ebe77f7b2a04ee56db2dcef8e899caddedbd9ee0
SereniT33 <90298024+SereniT33@users.noreply.github.com>
2021-12-02 01:09:43 -0800

    vectors identified

--- census.R
+++ census.R
@@ -20,7 +20,7 @@ kelownaCMA16 <- list_census_regions('CA16') %>%
 #"kelownaCT16" below is census tract data for Kelowna according to 2016 census data.
 
 kelownaCT16 <- get_census(dataset='CA16', regions=list(CMA='59915'),
-          vectors=c("v_CA16_408","v_CA16_409","v_CA16_410"),
+          vectors=c("v_CA16_408","v_CA16_409","v_CA16_410", "v_CA16_7", "v_CA16_244"),
           level = 'CT', use_cache = FALSE)
 
 #The dataset above includes census tracts in WK and the first nation.
@@ -44,7 +44,66 @@ kelownaDA16 <- get_census(dataset='CA16', regions=list(CMA='59915'),
                           level = 'DA', use_cache = FALSE)
 
 
-#Now I will look at age variables for CoK. I want to get data for
-# age groups, 0~4 & 65+.
+#Now I will find vector #'s for each variables.
+
+#First, we will find the vector #'s for age.
+#age "0 to 4 years" is "v_CA16_7".
+#age "65 years and over" is v_CA16_244.
 
 find_census_vectors("0 to 4 years", dataset = "CA16", type = "total", query_type = "exact")
+
+find_census_vectors("65 years", dataset = "CA16", type = "total", query_type = "exact")
+
+find_census_vectors("0 to 4 years", dataset = "CA16", type = "total", query_type = "exact")
+
+#Second is the visible minority population. (vector# v_CA16_3957)
+
+find_census_vectors("visible minority", dataset = "CA16", type = "total", query_type = "exact")
+
+#Third is the knowledge of language - "neither English nor French".
+# (vector# V_CA16_524) While I chose 524, vector ~539 also shows up.
+
+find_census_vectors("neither English nor", dataset = "CA16", type = "total", query_type = "exact")
+
+#Fourth, income less than
+
+
+#Fifth, education attainment, "no certificate, diploma or degree"
+# vector# v_CA16_5054. (also note, 5099)
+find_census_vectors("no certificate, diploma", dataset = "CA16", type = "total", query_type = "exact")
+
+#Sixth, primary/tourism industry. These industries include
+#"Agriculture, forestry, fishing and hunting" (v_CA16_5702),
+#"Mining, quarrying, and oil and gas extraction" (v_CA16_5705),
+#for the primary industries, and
+#"Transportation and warehousing" (v_CA16_5723),
+#"Arts, entertainment and recreation" (v_CA16_5750),
+#"Accommodation and food services" (v_CA16_5753)
+#for tourism.
+find_census_vectors("Agriculture, forestry", dataset = "CA16", type = "total", query_type = "exact")
+find_census_vectors("Mining, quarrying, and oil", dataset = "CA16", type = "total", query_type = "exact")
+find_census_vectors("Transportation and warehousing", dataset = "CA16", type = "total", query_type = "exact")
+find_census_vectors("Arts, entertainment", dataset = "CA16", type = "total", query_type = "exact")
+find_census_vectors("Accommodation and food", dataset = "CA16", type = "total", query_type = "exact")
+
+
+#Seventh, home ownership data has
+#"Owner" (v_CA16_4837) & "Renter" (v_CA16_4838)
+find_census_vectors("Owner", dataset = "CA16", type = "total", query_type = "exact")
+find_census_vectors("Renter", dataset = "CA16", type = "total", query_type = "exact")
+
+#Eighth, structure types of dwelling include
+#"Single-detached house" (v_CA16_40),
+#"Apartment in a building that has five or more storeys" (v_CA16_410),
+#"Movable dwelling" (v_CA16_417).
+
+find_census_vectors("Single-detached house", dataset = "CA16", type = "total", query_type = "exact")
+find_census_vectors("Apartment in a building that has five or more storeys", dataset = "CA16", type = "total", query_type = "exact")
+find_census_vectors("Movable dwelling", dataset = "CA16", type = "total", query_type = "exact")
+
+#Ninth, living alone is determined by
+# household size of "1 person". (v_CA16_419)
+
+find_census_vectors("1 person", dataset = "CA16", type = "total", query_type = "exact")
+
+
